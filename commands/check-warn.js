ad5ad29d585d35e4532027a09cc4c35ee7fe1edd
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-01-22 14:21:07 +0700

    Create check-warn.js

---
+++ commands/check-warn.js
@@ -0,0 +1,13 @@
+module.exports = {
+  
+  name: "checkwarn",
+  code: `
+$title[__**WARN CHECK**__]
+$description[**$username[$mentioned[1]]#$discriminator[$mentioned[1]] has $getServerVar[warn;$mentioned[1]] Warnings**]
+$footer[Requested by @$username]
+$color[d7342a]
+$onlyIf[$mentioned[1]!=;**Mention User!**
+__**Example**__
+\`$getServerVar[prefix]checkwarn <@user>\` ]
+$onlyPerms[admin;❌ **You need Administrator Permissions!**]`
+}
