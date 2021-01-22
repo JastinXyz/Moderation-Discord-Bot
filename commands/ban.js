566ce8437545deb54f6f6daa5ce3284d78e09c91
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-01-22 14:18:47 +0700

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,17 @@
+module.exports = {
+  
+  name: "ban",
+  code: `
+$ban[$mentioned[1]]
+$color[#FF0000]
+$footer[Success Banned User]
+$author[Its a bird, Its a plane, Its a ban hammer!]
+$description[Ban hammer succesfully used!
+$addField[Banned;<@$mentioned[1]>]
+$addField[Admin;<@$authorID>]
+$addField[Reason;$noMentionMessage]
+]
+$argsCheck[>2;Use: \`$getServerVar[prefix]ban <@user> <Reason>\`]
+$onlyPerms[admin;❌ **You need Administrator Permissions!**]
+`
+}
